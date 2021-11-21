bc7f57132c1d31541ae20c88352c0c18c9bedc88
Moros <71281300+Moros0741@users.noreply.github.com>
2021-11-21 17:01:18 -0500

    chore: create requests class

--- src/PyGuard/PyGuard.py
+++ src/PyGuard/PyGuard.py
@@ -1,7 +1,55 @@
-import aiohttp
+import aiohttp, typing, codecs, io
 from aiohttp import ClientSession
+from typing import Optional
+from codecs import decode, encode
+from io import BytesIO
+import logging
 
-class Guardian:
+BASE_URL = "http://api.guardiansystem.xyz/v1"
 
-    async def fetchOffender(self, userId: int):
-        pass
+log = logging.getLogger(__name__)
+
+class GuardClient:
+    """
+    PyGuard
+    -
+    A Python wrapper for Discord Guardian System API
+    """
+    def __init__(self):
+        self.credentials = None
+
+    def login(self, API_Token: str, id: str):
+        """
+        login to the API using your credentials. 
+    
+        You can apply for credentials at https://guardiansystem.xyz. \n
+        For security, It is recommended that login information is stored
+        in a `.env` file.
+
+        ~@`paramaters`:
+            `token`: str (required) Your accesss token granted from application 
+            `id`: str (required) Your Application's Discord ID as string.
+ 
+        ```
+        GuardClient.login(
+            id=os.getenv("CLIENT_ID")
+            API_Token=os.getenv("API_TOKEN")
+        )
+        ```
+        """
+
+        strencode = encode(bytes(id + ":" + API_Token, "utf-8"), 'base64')
+        self.credentials = decode(strencode, "utf-8")
+
+        
+    def credentials(self): 
+        """
+        fetches the API_token & Client Id input in `.login()`
+        """
+        return self.credentials
+
+
+class Request:
+    pass
+       
+
